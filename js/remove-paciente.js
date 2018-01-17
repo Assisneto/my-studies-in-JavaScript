665b3fd4469160a8ed67d6db5330be98918632af
Assisneto <netodamasceno12@gmail.com>
2018-01-17 10:19:00 -0200

    My studies on javascript

---
+++ js/remove-paciente.js
@@ -0,0 +1,12 @@
+var tabela = document.querySelector('table');
+
+tabela.addEventListener('dblclick', function(event) {
+
+    // Somente executará nosso código caso o elemento em que clicamos seja um <td>
+    if (event.target.tagName == 'TD') {
+        event.target.parentNode.classList.add("fadeOut");
+        setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 450);
+    }
+});
